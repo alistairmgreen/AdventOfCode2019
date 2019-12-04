4e97694a43018318aea35c6c11195b1af065811d
Alistair Green <alistairmgreen@gmail.com>
2019-12-04 21:54:30 +0000

    Day 4, part 2.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -11,19 +11,27 @@ fn main() {
        This implies that the smallest allowed code is 248888 and the largest
        is 699999.
     */
+
+    let mut all_solutions = Vec::new();
     
-    let mut num_codes = continuations(&[2, 4, 8], 6)
-        + continuations(&[2, 4, 9], 6);
+    let mut num_codes = continuations(&[2, 4, 8], 6, &mut all_solutions)
+        + continuations(&[2, 4, 9], 6, &mut all_solutions);
     
     for n in 5..=9 {
-        num_codes += continuations(&[2, n], 6);
+        num_codes += continuations(&[2, n], 6, &mut all_solutions);
     }
 
     for n in 3..=6 {
-        num_codes += continuations(&[n], 6);
+        num_codes += continuations(&[n], 6, &mut all_solutions);
     }
 
     println!("There are {} possible codes.", num_codes);
+    
+    let part2 = all_solutions.iter()
+        .filter(|code| has_digit_appearing_exactly_twice(code))
+        .count();
+    
+    println!("{} of these have a digit that appears exactly twice.", part2);
 }
 
 fn has_repeats(code: &[u8]) -> bool {
@@ -36,7 +44,16 @@ fn has_repeats(code: &[u8]) -> bool {
     false
 }
 
-fn continuations(code: &[u8], length: usize) -> u32 {
+fn has_digit_appearing_exactly_twice(code: &[u8]) -> bool {
+    let mut digit_frequency = vec![0; 10];
+    for digit in code {
+        digit_frequency[*digit as usize] += 1;
+    }
+
+    digit_frequency.into_iter().any(|n| n == 2)
+}
+
+fn continuations(code: &[u8], length: usize, all_solutions: &mut Vec<Vec<u8>>) -> u32 {
     let digit_count = code.len();
     let last_digit = code[digit_count - 1];
 
@@ -47,7 +64,7 @@ fn continuations(code: &[u8], length: usize) -> u32 {
             for next_digit in last_digit..=9 {
                 let mut next_code = code.to_owned();
                 next_code.push(next_digit);
-                println!("{:?}", next_code);
+                all_solutions.push(next_code);
             }
 
             return 10 - last_digit as u32;
@@ -55,7 +72,7 @@ fn continuations(code: &[u8], length: usize) -> u32 {
             // Last digit has to be same as penultimate
             let mut next_code = code.to_owned();
             next_code.push(last_digit);
-            println!("{:?}", next_code);
+            all_solutions.push(next_code);
 
             return 1;
         }
@@ -63,12 +80,11 @@ fn continuations(code: &[u8], length: usize) -> u32 {
 
     let mut possibilities = 0;
     
-
     for next_digit in last_digit..=9 {
         let mut next_code = code.to_owned();
         next_code.push(next_digit);
 
-        possibilities += continuations(&next_code, length);
+        possibilities += continuations(&next_code, length, all_solutions);
     }
 
     possibilities
