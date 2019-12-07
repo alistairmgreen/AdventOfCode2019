bd9a0a84d505b2ab5ab3175ca52d43cf29566f4f
Alistair Green <alistairmgreen@gmail.com>
2019-12-07 17:22:44 +0000

    Day 7, part 1.

---
+++ day7/src/lib.rs
@@ -0,0 +1,56 @@
+pub mod permutation {
+    pub struct Permutations<T> {
+        items: Vec<T>,
+        stack: Vec<usize>,
+        counter: usize,
+        is_first: bool,
+    }
+
+    impl<T> Permutations<T> {
+        pub fn of(items: Vec<T>) -> Permutations<T>{
+            let n = items.len();
+            Permutations {
+                items: items,
+                stack: vec![0; n],
+                counter: 0,
+                is_first: true,
+            }
+        }
+    }
+
+    impl<T> Iterator for Permutations<T>
+    where T: Clone
+    {
+        type Item = Vec<T>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            // Heap's Algorithm for permutations of a list.
+            // https://en.wikipedia.org/wiki/Heap%27s_algorithm
+            
+            if self.is_first {
+                self.is_first = false;
+                return Some(self.items.clone());
+            }
+
+            while self.counter <= self.stack[self.counter] {
+                self.stack[self.counter] = 0;
+                self.counter += 1;
+
+                if self.counter >= self.items.len() {
+                    return None;
+                }
+            }
+
+            if self.counter % 2 == 0 {
+                self.items.swap(0, self.counter);
+            } else {
+                self.items.swap(self.stack[self.counter], self.counter)
+            }
+
+            self.stack[self.counter] += 1;
+            self.counter = 0;
+
+            Some(self.items.clone())
+        }
+    }
+}
