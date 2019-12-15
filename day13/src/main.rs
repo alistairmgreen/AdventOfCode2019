7532c74bdb0166f2052c6967b9ca95283ddaff87
Alistair Green <alistairmgreen@gmail.com>
2019-12-15 23:35:27 +0000

    Day 13, Part 2.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,53 +1,117 @@
-use intcode::{IntcodeMachine, ProgramState};
-use std::fmt;
-use std::collections::HashMap;
-use std::io::{Write, stdout};
-use crossterm::{QueueableCommand, cursor, execute, queue, terminal, style};
 use crossterm::event::{read, Event};
+use crossterm::{cursor, execute, queue, style, terminal};
+use intcode::{IntcodeMachine, ProgramState};
+use std::cmp::Ordering;
+use std::{fmt, thread, time::Duration};
+use std::io::{stdout, Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut machine: IntcodeMachine = include_str!("puzzle_input.txt").parse()?;
-    let mut screen: HashMap<(u16, u16), Tile> = HashMap::new();
-
-    match machine.run()? {
-        ProgramState::Completed(output) => {
-            let tiles = output.chunks(3);
-            for tile in tiles {
-                let position = (tile[0] as u16, tile[1] as u16);
-                let tile_type = match tile[2] {
-                    0 => Tile::Empty,
-                    1 => Tile::Wall,
-                    2 => Tile::Block,
-                    3 => Tile::Paddle,
-                    4 => Tile::Ball,
-                    other => panic!("Unexpected tile type {}", other)
-                };
-
-                screen.insert(position, tile_type);
-            }
-        }
-        ProgramState::PendingInput(_) => {
-            eprintln!("Program is waiting for input.");
-        }
-    }
+    let mut ball_x = 0;
+    let mut paddle_x = 0;
+    let mut score = 0;
 
     let mut stdout = stdout();
-    queue!(stdout, terminal::Clear(terminal::ClearType::All), cursor::Hide)?;
+    execute!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::Hide
+    )?;
 
-    for (&(x, y), &tile_type) in screen.iter() {
-        queue!(
-            stdout,
-            cursor::MoveTo(x, y),
-            style::Print(tile_type.to_string())
-        )?;
+    'game: loop {
+        match machine.run()? {
+            ProgramState::Completed(output) => {
+                let tiles = output.chunks(3);
+                for tile in tiles {
+                    if tile[0] < 0 {
+                        score = tile[2];
+
+                        execute!(
+                            stdout,
+                            cursor::MoveTo(43, 0),
+                            style::Print(format!("Score: {:>6}", score))
+                        )?;
+
+                        continue;
+                    }
+
+                    let position = (tile[0] as u16, tile[1] as u16);
+                    let tile_type = match tile[2] {
+                        0 => Tile::Empty,
+                        1 => Tile::Wall,
+                        2 => Tile::Block,
+                        3 => Tile::Paddle,
+                        4 => Tile::Ball,
+                        other => panic!("Unexpected tile type {}", other),
+                    };
+
+                    queue!(
+                        stdout,
+                        cursor::MoveTo(position.0, position.1),
+                        style::Print(tile_type.to_string())
+                    )?;
+                }
+                stdout.flush()?;
+
+                break 'game;
+            }
+            ProgramState::PendingInput(output) => {
+                let tiles = output.chunks(3);
+                for tile in tiles {
+                    if tile[0] < 0 {
+                        score = tile[2];
+
+                        execute!(
+                            stdout,
+                            cursor::MoveTo(43, 0),
+                            style::Print(format!("Score: {:>6}", score))
+                        )?;
+
+                        continue;
+                    }
+
+                    let position = (tile[0] as u16, tile[1] as u16);
+
+                    let tile_type = match tile[2] {
+                        0 => Tile::Empty,
+                        1 => Tile::Wall,
+                        2 => Tile::Block,
+                        3 => Tile::Paddle,
+                        4 => Tile::Ball,
+                        other => panic!("Unexpected tile type {}", other),
+                    };
+
+                    if tile_type == Tile::Ball {
+                        ball_x = position.0;
+                    }
+
+                    if tile_type == Tile::Paddle {
+                        paddle_x = position.0;
+                    }
+
+                    execute!(
+                        stdout,
+                        cursor::MoveTo(position.0, position.1),
+                        style::Print(tile_type.to_string())
+                    )?;
+                }
+
+                thread::sleep(Duration::from_millis(10));
+
+                match paddle_x.cmp(&ball_x) {
+                    Ordering::Less => {
+                        machine.add_input(1);
+                    }
+                    Ordering::Equal => {
+                        machine.add_input(0);
+                    }
+                    Ordering::Greater => {
+                        machine.add_input(-1);
+                    }
+                }
+            }
+        }
     }
-
-    let block_count = screen.values()
-        .filter(|&&tile| tile == Tile::Block)
-        .count();
-    
-
-    stdout.flush()?;
 
     loop {
         if let Event::Key(_) = read()? {
@@ -55,9 +119,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    execute!(stdout, terminal::Clear(terminal::ClearType::All), cursor::Show)?;
+    execute!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::Show
+    )?;
 
-    println!("There are {} block tiles.", block_count);
+    println!("Final score: {}", score);
 
     Ok(())
 }
@@ -78,7 +146,7 @@ impl fmt::Display for Tile {
             Tile::Wall => write!(f, "X"),
             Tile::Block => write!(f, "*"),
             Tile::Paddle => write!(f, "_"),
-            Tile::Ball => write!(f, "O")
+            Tile::Ball => write!(f, "O"),
         }
     }
 }
